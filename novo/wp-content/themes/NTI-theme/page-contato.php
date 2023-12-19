bae6bb47c7ed2c5f8f2ec4ad1a4b7d28167aac98
IgorMozetic <igor.mozetic@ufabc.edu.br>
2023-12-19 14:00:59 -0300

    feat: create page-contato.php and adjust structure and style in page-mapa-so-site.php

---
+++ novo/wp-content/themes/NTI-theme/page-contato.php
@@ -0,0 +1,53 @@
+<?php
+get_header();
+?>
+<div id="content" class="wrap">
+  <div id="primary" class="content-area py-2">
+    <main id="main" class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-12">
+          <div class="breadcrumb bg-light pt-3 m-0"><?php get_breadcrumb(); ?></div>
+          <h1 class="wp-block-heading"><strong>Atendimento ao público</strong></h1>
+          <p><strong>Está com dificuldades para acessar algum sistema, ou recurso de TIC da UFABC? </strong></p>
+          <p><strong>Acesse a&nbsp;Central de Serviços, registrando a sua solicitação&nbsp;<a href="https://central.ufabc.edu.br/">aqui</a>.</strong></p>
+          <p>De forma a melhor organizar a prestação de serviços à Comunidade Acadêmica o NTI presta atendimento EXCLUSIVAMENTE através de agendamento via Central de Serviços. </p>
+          <p>Para demais dúvidas e questionamentos gerais, favor entrar em contato via e-mail:&nbsp;<a href="mailto:suporte@ufabc.edu.br">suporte@ufabc.edu.br</a></p>
+          <br>
+          <h2 class="wp-block-heading"><strong>Coordenação Geral</strong></h2>
+          <p>Coordenador atual: Paulo Victor Fernandes da Silva</p>
+          <p>Email:&nbsp;<a href="mailto:paulo.victor@ufabc.edu.br">paulo.victor@ufabc.edu.br</a></p>
+          <br>
+          <h2 class="wp-block-heading"><strong>Coordenação de Negócios e Soluções</strong></h2>
+          <p>Coordenador atual: Fabio Neves Margarido&nbsp;&nbsp;&nbsp;</p>
+          <p>Email:&nbsp;<a href="mailto:fabio.margarido@ufabc.edu.br">fabio.margarido@ufabc.edu.br</a></p>
+          <br>
+          <h2 class="wp-block-heading"><strong>Coordenação de Gestão e Planejamento</strong></h2>
+          <p>Coordenador atual: Sergio Roberto Meneses de Carvalho&nbsp;&nbsp;&nbsp;</p>
+          <p>Email:&nbsp;<a href="mailto:sergio.carvalho@ufabc.edu.br">sergio.carvalho@ufabc.edu.br</a></p>
+          <br>
+          <h2 class="wp-block-heading"><strong>Coordenação de Operações e Serviços&nbsp;</strong></h2>
+          <p>Coordenador atual: Carlos Alberto Orsolon da Silva</p>
+          <p>Email:&nbsp;<a href="mailto:carlos.alberto@ufabc.edu.br">carlos.alberto@ufabc.edu.br</a></p>
+          <br>
+          <h2 class="wp-block-heading"><strong>Divisão de Data Center</strong></h2>
+          <p>Chefia atual: Cristiano de Noronha Lopes</p>
+          <br>
+          <h2 class="wp-block-heading"><strong>Divisão de Redes</strong></h2>
+          <p>Chefia atual:&nbsp;Samuel Werneck</p>
+          <br>
+          <h2 class="wp-block-heading"><strong>Divisão de Suporte</strong></h2>
+          <p>Chefia atual: Felipe Baena Garcia</p>
+          <h4 class="wp-block-heading"><strong>Seção de Suporte – Santo André</strong></h4>
+          <p>Chefia atual: Mailsom José de Souza</p>
+          <h4 class="wp-block-heading"><strong>Seção de Suporte – São Bernardo do Campo</strong></h4>
+          <p>Chefia atual: Flávio Archanjo Carramaschi</p>
+          <p></p>
+        </div><!-- .col-8 -->
+      </div>
+    </main>
+  </div><!-- row -->
+  <!-- #main -->
+</div>
+<?php
+get_footer();
+?>
