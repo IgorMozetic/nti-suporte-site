a1362481db50dbe81f6e2e7f391ce9ff484c9604
IgorMozetic <igor.mozetic@ufabc.edu.br>
2023-12-20 12:19:55 -0300

    feat: create page of sobre o NTI

---
+++ novo/wp-content/themes/NTI-theme/page-sobre-o-nti.php
@@ -0,0 +1,15 @@
+<?php
+get_header(); ?>
+<div class="col-md-12">
+  <div class="container">
+    <div class="breadcrumb bg-light pt-3"><?php get_breadcrumb(); ?></div>
+    <h1 class="title">Sobre o Núcleo de Tecnologia da Informação</h1>
+    <div class="div-menus pt-4">
+      <p>Acesse a página de nossos <a href="http://localhost/wordpress-site/novo/objetivos-missao-visao-valores/">Objetivos, Missão, Visão e Valores</a></p>
+      <p>Acesse a página de nossa <a href="http://localhost/wordpress-site/novo/estrutura-organizacional/">Estrutura Organizacional</a></p>
+    </div>
+  </div>
+</div>
+<div class="footer-page-small">
+  <?php get_footer(); ?>
+</div>
