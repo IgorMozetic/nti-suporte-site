6040f2ff4808899f3259f172c5f5e62a5b2a4609
IgorMozetic <igor.mozetic@ufabc.edu.br>
2023-12-20 12:18:57 -0300

    feat: create page of estrutura organizacional o NTI

---
+++ novo/wp-content/themes/NTI-theme/page-estrutura-organizacional.php
@@ -0,0 +1,81 @@
+<?php
+get_header(); ?>
+<div id="content" class="wrap">
+  <div id="primary" class="content-area">
+    <main id="main" class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-12">
+          <div class="breadcrumb bg-light pt-3"><?php get_breadcrumb(); ?></div>
+          <div class="coordenacao-geral pb-3">
+            <h3><strong>Coordenação Geral do NTI</strong></h3>
+            <p>A Coordenação Geral do NTI é responsável pelo planejamento, gestão e coordenação do Núcleo de Tecnologia da Informação da UFABC.</p>
+            <p><strong>Responsável: </strong>Paulo Victor Fernandes da Silva</p>
+            <p><strong>E-mail: </strong><a href="mailto:paulo.victor@ufabc.edu.br">paulo.victor@ufabc.edu.br</a> </p>
+          </div>
+          <div class="coordenacao-negocios py-4">
+            <h3><strong>Coordenação de Negócios e Soluções</strong></h3>
+            <p>Coordena a gestão das demandas e a entrega de soluções em automação de processos de negócios da universidade, atua no levantamento e definição dos processos, especificação de requisitos de negócio e de software e desenvolvimento e manutenção dos sistemas de informação.Mantém um ambiente de gestão de negócios e soluções de TIC, que propicie a integração sistêmica das atividades de levantamento, tratamento e documentação técnica da comunidade acadêmica.</p>
+            <p><strong>Responsável:</strong> Fábio Neves Margarido</p>
+            <p><strong>E-mail: </strong><a href="mailto:fabio.margarido@ufabc.edu.br">fabio.margarido@ufabc.edu.br</a></p>
+            <div class="desenvolviemnto-solucoes py-3">
+              <h4>Desenvolvimento de Soluções</h4>
+              <p>Sua competência básica é o modelamento e codificação de sistemas. Implementa os casos de uso do projeto que está sendo executado e realiza a integração dos componentes e módulos do sistema. Faz gestão de versões e administra objetos corporativos reutilizáveis. Faz administração, configuração, suporte e manutenção dos sistemas acadêmicos e administrativos desenvolvidos internamente.</p>
+            </div>
+            <div class="gestao-dados py-3">
+              <h4>Gestão de Bases de Dados</h4>
+              <p>Manipula as estruturas e procedimentos necessários ao armazenamento, recuperação e exclusão de dados. Faz a gestão da integridade e da unicidade dos dados e informações das bases de dados institucionais. Fornece apoio técnico ao Desenvolvimento de Soluções de software, no que concerne a modelagem e gestão de bases de dados.</p>
+            </div>
+            <div class="gestao-negocios py-3">
+              <h4>Gestão de Negócios de TI</h4>
+              <p>Tem por competência mediar a comunicação entre a área requisitante de solução e a área de TI.Traduz as necessidades de negócio para uma visão organizada e técnica, própria da área de tecnologia da informação; analisa os processos de negócios do requisitante, faz o levantamento, análise e especificação de requisitos; levanta as necessidades do usuário e as formaliza em documentos técnicos que nortearão o desenvolvimento ou a manutenção de um sistema de informação.São suas atribuições: a criação de documentação de requisitos, de erros e funcionalidades viciadas, modelagem dos sistemas, diagramação, casos de uso, relacionamentos e fluxos de dados.Faz, também, a gestão das demandas, análise de alternativas, estudo de viabilidade, planejamento dos sistemas e serviços, análise e controle do portfólio de serviços de TI.</p>
+            </div>
+          </div>
+          <div class="coordenacao-operacoes py-4">
+            <h3><strong>Coordenação de Operações e Serviços</strong></h3>
+            <p>Propõe e coordena o desenvolvimento e a implantação de soluções de tecnologia da informação que dão suporte aos processos de atividades-fim da UFABC.Gerencia os sistemas informatizados, serviços de comunicação, acesso a rede local e internet e integração de sistemas de hardware e software.</p>
+            <p><strong>Responsável: </strong>Carlos Alberto Orsolon Silva</p>
+            <p><strong>E-mail: </strong><a href="mailto:carlos.alberto@ufabc.edu.br">carlos.alberto@ufabc.edu.br</a></p>
+            <div class="divisao-datacenter py-3">
+              <h4>Divisão de Data Center</h4>
+              <p><strong>Responsável:</strong> Cristiano de Noronha Lopes</p>
+              <p><strong>E-mail: </strong><a href="mailto:datacenter.adm@ufabc.edu.br">datacenter.adm@ufabc.edu.br</a></p>
+              <p>É o setor responsável por manter os sistemas administrativos em funcionamento. As tarefas executadas se baseiam na manutenção do parque de máquinas responsável por hospedar os serviços administrativos da UFABC (DHCP, DNS, TIDIA, servidores de páginas, Serviços da Biblioteca, Sistema Acadêmico, Backups, Bancos de Dados e outros). Também é responsabilidade do setor a alocação de recursos para novos serviços mediante a disponibilidade desses recursos e a importância que o mesmo tem mediante ao uso.</p>
+            </div>
+            <div class="divisao-redes py-3">
+              <h4>Divisão de Redes</h4>
+              <p><strong>Responsável:</strong> Samuel Werneck</p>
+              <p><strong>E-mail: </strong><a href="mailto:infra.nti@ufabc.edu.br">infra.nti@ufabc.edu.br</a></p>
+              <p>Instala, mantém e gerencia a infraestrutura física e lógica da rede de dados e voz e seus subsistemas de apoio. Gerencia e monitora os serviços de acesso à internet. Presta atendimento de suporte de segundo nível na utilização dos serviços de acesso a rede.</p>
+            </div>
+            <div class="divisao-suporte py-3">
+              <h4>Divisão de Suporte</h4>
+              <p><strong>Responsável:</strong> Felipe Baena Garcia</p>
+              <p><strong>E-mail: </strong><a href="mailto:suporte@ufabc.edu.br">suporte@ufabc.edu.br</a></p>
+              <p>Gerencia, planeja e coordena as atividades da área de serviços e suporte técnico aos usuários, compreendendo o desenvolvimento, aperfeiçoamento e uso da integração de hardwares e softwares, aperfeiçoando a forma e eficiência de atendimento. Propõe melhorias nos sistemas operacionais dos equipamentos e microcomputadores dos usuários. Estabelece regras e padrões para o uso da Central de Serviços, Sistema de Tickets e Portal do NTI, integrando de forma aprimorada os serviços de TI com a comunidade. </p>
+              <p>Instala, mantém e recolhe equipamentos de informática e periféricos nas salas de aula, laboratórios, salas de professores e áreas administrativas da universidade. Presta apoio técnico aos alunos, funcionários e professores, abrangendo a solução de problemas de hardware e software ocorridos no uso dos equipamentos de TI da universidade. </p>
+              <h4><strong>Seção de Suporte – Santo André</strong></h4>
+              <p><strong>Responsável:</strong> Mailsom José de Souza</p>
+              <h4><strong>Seção de Suporte – São Bernardo do Campo</strong></h4>
+              <p><strong>Responsável:</strong> Flávio Archanjo Carramaschi</p>
+            </div>
+          </div>
+          <div class="coordenacao-gestao py-4">
+            <div class="gestao">
+              <h3><strong>Coordenação de Gestão e Planejamento</strong></h3>
+              <p>Coordena a gestão e o planejamento das demandas de aquisições de equipamentos, serviços e suprimentos, que dão suporte aos processos de atividades-fim da UFABC. Gerencia os estoques de equipamentos e suprimentos, acompanha a fiscalização dos contratos em tecnologia da informação. Responsável pelo planejamento estratégico de tecnologia da informação, mapeamento de processos, acompanhamento de projetos em tecnologia da informação e levantamento de demandas de capacitação dos servidores do Núcleo de Tecnologia da Informação.</p>
+              <p><strong>Responsável:</strong> Sérgio Roberto Meneses de Carvalho</p>
+              <p><strong>E-mail: </strong><a href="mailto:sergio.carvalho@ufabc.edu.br">sergio.carvalho@ufabc.edu.br</a></p>
+              <p><strong><a rel="noreferrer noopener" href="https://docs.google.com/spreadsheets/d/e/2PACX-1vTZGm9ewqhU4gCAaIJhf5gkeaCD0R-k-GPA0NcabOIHqabL71b9gH28u9Mm9QiuKw/pubhtml?gid=2003521629&amp;single=true&amp;widget=true&amp;headers=false" target="_blank">Clique aqui</a> para ver a escala de teletrabalho da equipe</strong></p>
+            </div>
+          </div>
+          <h3><strong>Organograma</strong></h3>
+          <figure class="wp-block-image"><img decoding="async" src="http://localhost/wordpress-site/novo/wp-content/uploads/2023/12/uploads/2023/05/unnamed-1.png" alt="">
+            <figcaption class="w-100 text-center">Figura. Organograma do NTI.</figcaption>
+          </figure>
+        </div>
+      </div>
+    </main>
+  </div>
+</div>
+
+<?php get_footer(); ?>
