648d06861e611bb5e4ab87c457ae795fe5125b28
IgorMozetic <igor.mozetic@ufabc.edu.br>
2023-12-20 12:19:36 -0300

    feat: create page of objetivos, missas, visao e valores o NTI

---
+++ novo/wp-content/themes/NTI-theme/page-objetivos.php
@@ -0,0 +1,48 @@
+<?php
+get_header(); ?>
+<div id="content" class="wrap">
+  <div id="primary" class="content-area">
+    <main id="main" class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-12">
+          <div class="breadcrumb bg-light pt-3"><?php get_breadcrumb(); ?></div>
+          <div class="objetivos pb-2">
+            <h1 class="has-text-align-left wp-block-heading">Objetivo</h1>
+            <p>O NTI tem como finalidade oferecer soluções estratégicas, táticas e operacionais de Tecnologia da Informação e Comunicação (TIC) à UFABC, considerando seus pilares acadêmicos: Ensino, Pesquisa e Extensão, que permitam a Universidade alcançar com excelência seus objetivos institucionais.</p>
+            <p><strong>Premissas para o funcionamento da área de TIC da UFABC, em alinhamento com os fundamentos da UFABC:</strong></p>
+            <ul>
+              <li>Ética e respeito, como condições imprescindíveis para o convívio humano e profissional;</li>
+              <li>Excelência no atendimento das demandas de TIC em consonância com os pilares da Universidade, abrangendo, portanto, pesquisa, ensino, extensão, inclusão e gestão;</li>
+              <li>Interdisciplinaridade, considerada como uma efetiva interação entre as áreas do conhecimento;</li>
+              <li>Busca constante por inovação, não como um fim em si, mas como a única forma de manter-se relevante perante rápidas mudanças da tecnologia e seu emprego;</li>
+              <li>Constante capacitação e qualificação dos servidores da área, visando ao atendimento das demandas da Universidade e ao desenvolvimento pessoal e à motivação dos colaboradores;</li>
+              <li>Gestão responsável e participativa dos recursos da Universidade, abrangendo recursos humanos, financeiros, patrimoniais e de espaço físico;</li>
+              <li>Responsabilidade ambiental e sustentabilidade.</li>
+            </ul>
+          </div>
+          <div class="missao py-2">
+            <h2 class="has-text-align-left wp-block-heading"><strong>Missão</strong></h2>
+            <p>"Oferecer soluções estratégicas, táticas e operacionais de TIC à UFABC, considerando seus pilares acadêmicos: Ensino, Pesquisa e Extensão, que permitam à Universidade alcançar com excelência seus objetivos institucionais."</p>
+          </div>
+          <div class="visao py-2">
+            <h2 class="has-text-align-left wp-block-heading"><strong>Visão</strong></h2>
+            <p>"Ser parceiro das diversas áreas da Universidade, prestando soluções de excelência no âmbito do ensino superior brasileiro através da prospecção, aquisição, desenvolvimento e sustentação das TIC, visando o aprimoramento permanente, alinhamento e integração às atividades-fim da UFABC aos processos de Gestão, Ensino, Pesquisa e Extensão."</p>
+          </div>
+          <div class="valores py-2">
+            <h2 class="has-text-align-left wp-block-heading"><strong>Valores</strong></h2>
+            <ul>
+              <li><strong>Integridade: </strong>Manter conduta ética, preservando a confiança dos usuários nos serviços prestados e na segurança de suas informações;</li>
+              <li><strong>Satisfação dos usuários:</strong> Preocupação em atender às demandas e necessidades da comunidade acadêmica com eficiência e eficácia, considerando a importância do sentimento de confiança e participação dos usuários na construção do relacionamento;</li>
+              <li><strong>Atualização e capacitação constantes:</strong> Zelo pela atualização constante dos seus servidores, propiciando meios para o cumprimento de suas atribuições, em especial considerando a dinâmica das mudanças na área de TI;</li>
+              <li><strong>Governança e Gestão: </strong>Procedimentos, documentos e processos mantidos atualizados e disponíveis, possibilitando a continuidade das atividades-fim e a comunicação transparente, interna e externamente;</li>
+              <li><strong>Profissionalismo:</strong> Comprometimento, dedicação, responsabilidade, competência, eficiência, eficácia, cordialidade e ética na atuação profissional;</li>
+              <li><strong>Colaboração: </strong>Promover ambiente propício à integração dos profissionais e realização conjunta dos trabalhos, favorecendo a criatividade, o compartilhamento das soluções e do conhecimento, através da sinergia de esforços, estimulação de capacidades múltiplas e da inteligência de grupo.</li>
+              <li><strong>Sustentabilidade</strong>: Prezar pelo bom uso dos recursos, desde a escolha dos itens e equipamentos, sua guarda, utilização, manutenção e descarte, evitando desperdícios e prezando pela economicidade.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</div>
+<?php get_footer(); ?>
