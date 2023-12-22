014a802d8d2646a80b7757a2115ad94f010e3633
IgorMozetic <igor.mozetic@ufabc.edu.br>
2023-12-22 14:58:00 -0300

    feat: change padding of breadcrumb

--- novo/wp-content/themes/NTI-theme/page-sobre-o-nti.php
+++ novo/wp-content/themes/NTI-theme/page-sobre-o-nti.php
@@ -2,7 +2,7 @@
 get_header(); ?>
 <div class="col-md-12">
   <div class="container">
-    <div class="breadcrumb bg-light pt-3"><?php get_breadcrumb(); ?></div>
+    <div class="breadcrumb bg-light pt-4"><?php get_breadcrumb(); ?></div>
     <h1 class="title">Sobre o Núcleo de Tecnologia da Informação</h1>
     <div class="div-menus pt-4">
       <p>Acesse a página de nossos <a href="http://localhost/wordpress-site/novo/objetivos-missao-visao-valores/">Objetivos, Missão, Visão e Valores</a></p>
