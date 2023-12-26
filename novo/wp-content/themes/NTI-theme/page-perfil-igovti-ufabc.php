baadb4e8c8d3b48ea7c183b2d7b7acec12de3b73
IgorMozetic <igor.mozetic@ufabc.edu.br>
2023-12-26 16:32:09 -0300

    feat: create page of perfil igovTI

---
+++ novo/wp-content/themes/NTI-theme/page-perfil-igovti-ufabc.php
@@ -0,0 +1,31 @@
+<?php
+get_header(); ?>
+<div id="content" class="wrap">
+  <div id="primary" class="content-area">
+    <main id="main" class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-12">
+          <div class="breadcrumb bg-light pt-4"><?php get_breadcrumb(); ?></div>
+          <h1>Perfil iGovTI UFABC</h1>
+          <div class="box-1-igovti py-3">
+            <p>Para avaliar a situação de governança de Tecnologia da Informação (TI) na Administração Pública Federal (APF), o Tribunal de Contas da União (TCU) tem realizado levantamentos baseados em questionários que abordam práticas de governança e de gestão de TI previstas em leis, regulamentos, normas técnicas e modelos internacionais de boas práticas.</p>
+            <p>O primeiro levantamento de governança de TI, realizado em 2007, contou com a participação de 255 organizações.</p>
+            <p>O índice de governança de TI (iGovTI) foi criado com o propósito de orientar as organizações públicas no esforço de melhoria da governança e da gestão de TI. O índice também permite ao TCU avaliar, de um modo geral, a efetividade das ações adotadas para induzir a melhoria da situação de governança de TI na APF.</p>
+          </div>
+          <div class="box-2-igovti pt-2 pb-4 d-flex flex-column justify-content-center align-items-center">
+            <div class="py-3">
+              <h4 class="text-center">Resultado da avaliação da UFABC no ciclo de 2016:</h4>
+              <figure class="size-full"><img decoding="async" src="https://nti.ufabc.edu.br/wp-content/uploads/2023/05/igov_2016.jpg" alt="" class="wp-image-4449" srcset="https://nti.ufabc.edu.br/wp-content/uploads/2023/05/igov_2016.jpg 649w, https://nti.ufabc.edu.br/wp-content/uploads/2023/05/igov_2016-300x152.jpg 300w" sizes="(max-width: 649px) 100vw, 649px"></figure>
+            </div>
+            <div class="pt-3">
+              <h4 class="text-center">Resultado da avaliação da UFABC no ciclo de 2014:</h4>
+              <figure class="size-full"><img decoding="async" loading="lazy" src="https://nti.ufabc.edu.br/wp-content/uploads/2023/05/igov_2014.jpg" alt="" class="wp-image-4450" srcset="https://nti.ufabc.edu.br/wp-content/uploads/2023/05/igov_2014.jpg 649w, https://nti.ufabc.edu.br/wp-content/uploads/2023/05/igov_2014-300x152.jpg 300w" sizes="(max-width: 649px) 100vw, 649px"></figure>
+            </div>
+            <p><small>Conheça os dados completos da UFABC no <a href="/wp-content/uploads/2023/05/iGovTI2016.pdf" target="_blank" rel="noreferrer noopener">Perfil GovTI2016</a>.</small></p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</div>
+<?php get_footer(); ?>
