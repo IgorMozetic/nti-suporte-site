f8e88c6a18828509d158322da500662b271c4030
IgorMozetic <igor.mozetic@ufabc.edu.br>
2023-12-27 18:10:23 -0300

    feat: create page of single aviso

---
+++ novo/wp-content/themes/NTI-theme/single-avisos.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+<div id="content" class="wrap">
+  <div id="primary" class="content-area">
+    <main id="main" class="container single-avisos">
+      <div class="breadcrumb bg-light pt-4"><?php get_breadcrumb(); ?></div>
+      <div class="row">
+        <div class="col-sm-12">
+          <?php
+          while (have_posts()) : the_post(); ?>
+            <?php $postid = get_the_ID(); ?>
+            <?php $category = get_the_category('$post->id') ?>
+            <?php
+            $terms = wp_get_post_terms($post->ID, 'category', array("fields" => "all")); ?>
+            <h1 class="page-title pb-4"><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+            <?php $categories = get_the_category(); ?>
+          <?php endwhile;
+          ?>
+          <div class="py-4">
+            <div class="d-flex justify-content-between">
+              <div><?php previous_post_link(); ?> </div>
+              <div><?php next_post_link(); ?></div>
+            </div>
+          </div>
+        </div>
+    </main>
+  </div>
+</div>
+
+
+<?php get_footer(); ?>
